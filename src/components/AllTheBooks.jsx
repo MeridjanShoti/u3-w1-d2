b585748510c8f38ba049bc7e957cc2bf10adcb22
MeridjanShoti <meridjanshoti@gmail.com>
2025-02-04 22:31:27 +0100

    a

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -1,21 +1,23 @@
-import { Button, Card } from "react-bootstrap";
+import { Button, Card, Col } from "react-bootstrap";
 import fantasy from "../assets/books/fantasy.json";
 import { Component } from "react";
 class AllTheBooks extends Component {
   render() {
     return (
-      <div>
+      <>
         {fantasy.map((book) => (
-          <Card key={`book-${book.asin}`} style={{ width: "18rem", display: "inline-block", verticalAlign: "top" }}>
-            <Card.Img variant="top" src={book.img} />
-            <Card.Body>
-              <Card.Title>{book.title}</Card.Title>
-              <Card.Text>{book.price}</Card.Text>
-              <Button variant="primary">Buy</Button>
-            </Card.Body>
-          </Card>
+          <Col key={`book-${book.asin}`} style={{ height: 450 }}>
+            <Card style={{ height: "100%" }}>
+              <Card.Img variant="top" src={book.img} style={{ height: "300px", objectFit: "contain" }} />
+              <Card.Body>
+                <Card.Title className="text-truncate">{book.title}</Card.Title>
+                <Card.Text>{book.price}</Card.Text>
+                <Button variant="primary">Buy</Button>
+              </Card.Body>
+            </Card>
+          </Col>
         ))}
-      </div>
+      </>
     );
   }
 }
--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -4,7 +4,7 @@ function MyFooter() {
       <footer className="text-center bg-secondary text-white">
         <p>Bookshop spa &copy;</p>
         <address>Via vattelappesca 69</address>
-        <a href="mailto:ciccio@pasticcio.com"></a>
+        <a href="mailto:ciccio@pasticcio.com">mailto:ciccio@pasticcio.com</a>
         <a href="tel:+393456789123">tel:+393456789123</a>
       </footer>
     </>
